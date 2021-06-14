58282051f49b47a399cce731a30e56a9cf374cbe
NashatAlzaatreh <>
2021-06-14 09:52:11 +0000

    practice

---
+++ app.js
@@ -0,0 +1,58 @@
+// // var firstName = "Batool "
+// // var lastName = '   Basam'
+
+// //  var finalName = firstName + lastName
+// //  console.log(finalName)
+
+// //  var x = 1
+// //  var y = 2 
+// //  console.log(x)
+
+// //  var name1 = 'nnn'
+// //  var name2 = 'zzz'
+// //  var full= name1 + name2
+// //  console.log(full)
+
+// //  document.write('<div>' + 'hello' + "</div>")
+
+// //  console.log('1' ==="1")
+ 
+// //  document.write('<h3>' + full + '</h3>')
+
+ 
+// //  var numberOfText = prompt('please enter the number of times you need to see welcome dear!')
+
+// // //  console.log(numberOfText)
+
+// //  document.write('<h1>' + numberOfText + '</h1>')
+// var i=1 ;
+// while(i < 7){
+//   document.write("<h4>"+"welcome dea!"+ "</h4>")
+//   i++
+//   console.log(i)
+//     i++
+
+// }
+
+
+
+// var x = 10;
+// var y = 1938;
+// function addTwoNumber(){
+
+// var sum = x + y ;
+// return sum;
+// }
+
+
+// console.log(addTwoNumber())
+// var x=1 , y= 2 , z = 3
+// function addTwoNumbers(x,y,z){
+
+// var sum = x + y ;
+// console.log(sum)
+// console.log(z)
+// return sum;
+// }
+ 
+//  addTwoNumbers(x,y,z)
